e6dbf3a289df43d3f757fbb523993b3dbd3979f1
dayanjr <dayanfrazao@byui.edu>
2023-12-22 18:41:30 -0600

    UP

--- server.js
+++ server.js
@@ -5,6 +5,7 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
@@ -14,7 +15,12 @@ const static = require("./routes/static")
  * Routes
  *************************/
 app.use(static)
-
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
